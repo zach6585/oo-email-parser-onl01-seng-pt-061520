694dd0da744fa4304ef6d274adc646d474298809
zachary mines <zmines@wesleyan.edu>
2020-07-12 20:24:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
   
   def parse
     if @email.include?(",")
-      @email.split(",").each do |emal|
+      @email.split(", ").each do |emal|
         if !@emails.include?(emal) 
           @emails << emal 
         end 
